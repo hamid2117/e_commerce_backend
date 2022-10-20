9d2aef1ba5ba4fcb73da4eaa4b43562fd0575c7c
hamid2117 <saim008865@gmail.com>
2022-10-20 10:03:42 +0500

    app js set

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('E-Commerce API');
+require('dotenv').config()
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+//database
+const connectDB = require('./db/connect')
+
+app.get('/', (req, res) => {
+  res.send('<h2>Welcome to ecommerce website')
+})
+//app.use('/api/v1')
+
+const Port = process.env.PORT || 3000
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGODBDEV)
+    app.listen(Port, console.log(`Server is listening on port : ${Port}`))
+  } catch (error) {
+    console.log(error)
+  }
+}
+start()
