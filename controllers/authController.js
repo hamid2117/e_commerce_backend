5d562591a9bddc97d6b0800786b4c86cfcd2afe4
hamid2117 <saim008865@gmail.com>
2022-10-21 06:38:24 +0500

    done making jwt token

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,20 @@
 const User = require('../models/User')
-const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
+const jwt = require('jsonwebtoken')
 
 const register = async (req, res) => {
   const { name, email, password } = req.body
 
   const role = (await User.countDocuments({})) === 0
 
-  res.status(StatusCodes.CREATED).json({ name, email, password, role })
+  const user = await User.create({ name, email, password })
+  const userToken = { name: user.name, _id: user._id, role: user.role }
+
+  const token = jwt.sign(userToken, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  })
+
+  res.status(StatusCodes.CREATED).json({ ...user })
 }
 const login = async (req, res) => {
   res.status(StatusCodes.OK).json({})
