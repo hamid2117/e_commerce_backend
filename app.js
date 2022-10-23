4fcda11b6a292f6299358939e818d8854c9d2bfe
hamid2117 <saim008865@gmail.com>
2022-10-23 11:00:27 +0500

    done basic structure for review

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const cors = require('cors')
 const authRouter = require('./routes/authRoutes')
 const userRouter = require('./routes/userRoutes')
 const productRouter = require('./routes/productRoutes')
+const reviewRouter = require('./routes/reviewRoutes')
+
 //database
 const connectDB = require('./db/connect')
 
@@ -35,6 +37,7 @@ app.get('/', (req, res) => {
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/products', productRouter)
+app.use('/api/v1/reviews', reviewRouter)
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
