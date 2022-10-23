b50b591c0dac619168c0bfd7bd72c7214d5ff92a
hamid2117 <saim008865@gmail.com>
2022-10-23 10:36:25 +0500

    done all crud of product

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,12 +2,22 @@ const Product = require('../models/Product')
 const { StatusCodes } = require('http-status-codes')
 const CustomError = require('../errors')
 
+// Remember all queries like sort etc
 const getAllProducts = async (req, res) => {
-  res.status(StatusCodes.OK).json({})
+  const products = await Product.find({})
+
+  res.status(StatusCodes.OK).json({ products })
 }
 
 const getSingleProduct = async (req, res) => {
-  res.status(StatusCodes.OK).json({})
+  const { id: productId } = req.params
+  const product = await Product.findById(productId)
+
+  if (!product) {
+    throw new CustomError.BadRequestError(`No product with id :${productId}`)
+  }
+
+  res.status(StatusCodes.OK).json({ product })
 }
 
 const createProduct = async (req, res) => {
@@ -18,11 +28,32 @@ const createProduct = async (req, res) => {
 }
 
 const updateProduct = async (req, res) => {
-  res.status(StatusCodes.OK).json({})
+  const { id: productId } = req.params
+
+  const product = await Product.findByIdAndUpdate(productId, req.body, {
+    runValidators: true,
+    new: true,
+  })
+
+  if (!product) {
+    throw new CustomError.BadRequestError(`No product with id :${productId}`)
+  }
+
+  res.status(StatusCodes.OK).json({ product })
 }
 
 const deleteProduct = async (req, res) => {
-  res.status(StatusCodes.OK).json({})
+  const { id: productId } = req.params
+
+  const product = await Product.findById(productId)
+
+  if (!product) {
+    throw new CustomError.BadRequestError(`No product with id :${productId}`)
+  }
+
+  product.delete()
+
+  res.status(StatusCodes.OK).json({ msg: 'Product is delete' })
 }
 
 const uploadImage = async (req, res) => {
