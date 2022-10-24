2b87d77d90daed0a2321a7a12fe0a4c3aab290e4
hamid2117 <saim008865@gmail.com>
2022-10-24 11:52:13 +0500

    review Model unique product and user

--- models/Review.js
+++ models/Review.js
@@ -1,18 +1,36 @@
 const mongoose = require('mongoose')
 
-const reviewSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, 'title of the review is required'],
-    maxlength: 100,
+const reviewSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'title of the review is required'],
+      trim: true,
+      maxlength: 100,
+    },
+    comment: {
+      type: String,
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+      required: [true, 'Please provide rating'],
+    },
+    user: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    product: {
+      type: mongoose.Types.ObjectId,
+      ref: 'Product',
+      required: true,
+    },
   },
-  comment: {
-    type: String,
-  },
-  rating: {
-    type: Number,
-    default: 0,
-  },
-})
+  { timestamps: true }
+)
+
+reviewSchema.index({ product: 1, user: 1 }, { unique: true })
 
 module.exports = mongoose.model('Review', reviewSchema)
