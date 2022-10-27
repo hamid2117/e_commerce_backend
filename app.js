ce1263b9adb2ad39a4aad396b6b29cee377b0284
hamid2117 <saim008865@gmail.com>
2022-10-27 06:39:56 +0500

    done implementing packages for security

--- app.js
+++ app.js
@@ -8,8 +8,13 @@ const app = express()
 //otherPackages
 const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
-const cors = require('cors')
 const fileUpload = require('express-fileupload')
+
+const cors = require('cors')
+const rateLimiter = require('express-rate-limit')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const mongoSanitize = require('express-mongo-sanitize')
 
 //routers
 const authRouter = require('./routes/authRoutes')
@@ -26,6 +31,19 @@ const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 const { authenticateUser } = require('./middleware/authentication')
 
+//rateLimiter
+app.use('trust proxy', 1)
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+  })
+)
+//
+app.use(helmet())
+app.use(xss())
+app.use(mongoSanitize())
+
 app.use(express.json())
 app.use(morgan('tiny'))
 app.use(cookieParser(process.env.JWT_SECRET))
@@ -35,11 +53,6 @@ app.use(express.static('./public'))
 app.use(fileUpload())
 
 //
-
-app.get('/', (req, res) => {
-  console.log(req.signedCookies)
-  res.send('<h2>Welcome to ecommerce website</h2>')
-})
 
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/users', userRouter)
