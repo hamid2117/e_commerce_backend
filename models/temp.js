33b9693612cd977d6b502ff43bc43292cd595cbe
hamid2117 <saim008865@gmail.com>
2022-11-11 19:05:00 +0500

    done including desc in app js

--- models/temp.js
+++ models/temp.js
@@ -20,17 +20,4 @@ const agg = [
       },
     },
   },
-];
-
-MongoClient.connect(
-  '',
-  { useNewUrlParser: true, useUnifiedTopology: true },
-  function (connectErr, client) {
-    assert.equal(null, connectErr);
-    const coll = client.db('').collection('');
-    coll.aggregate(agg, (cmdErr, result) => {
-      assert.equal(null, cmdErr);
-    });
-    client.close();
-  }
-);
+]
