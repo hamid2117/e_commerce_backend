270e553b0695490c534e5f93a3743e3bced997ff
hamid2117 <saim008865@gmail.com>
2023-02-16 20:39:20 +0500

    done comments

--- utils/checkPermissions.js
+++ utils/checkPermissions.js
@@ -1,5 +1,5 @@
 const CustomError = require('../errors')
-
+// checking permissions of the route
 const chechPermissions = (requestUser, resourceUserId) => {
   if (requestUser.role === 'admin') return
   if (requestUser.userId === resourceUserId.toString()) return
